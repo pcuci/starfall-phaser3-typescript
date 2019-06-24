bfc7e21ea77a0e0106e3291174975e032db3ddbb
Paul Cuciureanu <paul.cuciureanu@ubisoft.com>
2019-06-24 19:32:40 -0400

    feat: new game waterfall

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { GameScene } from "./gameScene";
 import { ScoreScene } from "./scoreScene";
 
 const config: GameConfig = {
-  title: "Starfall",
+  title: "Waterfall",
   width: 800,
   height: 600,
   parent: "game",
